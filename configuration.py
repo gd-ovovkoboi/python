11f7d7c43cb2a653f1ffe5f7909cee9216fd3097
Oleksii Vovkoboi <ovovkoboi@griddynamics.com>
2020-05-11 08:43:44 +0200

    - add config.ini; - rework logging config;

--- configuration.py
+++ configuration.py
@@ -1,39 +1,17 @@
-import os, logging
+import os, logging, json
 from logging.config import dictConfig
+from configparser import ConfigParser
 
 from flask import Flask
 
-dictConfig({
-    'version': 1,
-    'formatters': {
-        'default': {
-            'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
-        }
-    },
-    'handlers': {
-        'wsgi': {
-            'class': 'logging.StreamHandler',
-            'stream': 'ext://flask.logging.wsgi_errors_stream',
-            'formatter': 'default'
-        },
-        'file': {
-            'class': 'logging.FileHandler',
-            'level': 'DEBUG',
-            'formatter': 'default',
-            'filename': 'app.log',
-            'mode': 'w',
-            'encoding': 'utf-8'
-        }
-    },
-    'root': {
-        'handlers': ['file', 'wsgi'],
-        'level': 'DEBUG'
-    }
-})
+with open('log_config.json') as f:
+    config_dict = json.load(f)
+    logging.config.dictConfig(config_dict)
+
+parser = ConfigParser()
+parser.read('config.ini')
 
 app = Flask(__name__)
-log = logging.getLogger('werkzeug')
-log.disabled = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.abspath(os.getcwd()) + '/database.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['JSON_SORT_KEYS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.abspath(parser.get('db', 'db_file'))
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = parser.getboolean('db', 'track_modifications')
+app.config['JSON_SORT_KEYS'] = parser.getboolean('rest', 'json_sort_keys')
--- decorators.py
+++ decorators.py
@@ -1,4 +1,4 @@
-import functools
+from functools import wraps
 
 from flask import request
 
@@ -6,13 +6,13 @@ from configuration import app
 
 
 def log_decorator(fn):
-    @functools.wraps(fn)
+    @wraps(fn)
     def decorated(*args, **kwargs):
         try:
             app.logger.debug('\n Method: {} \n URL: {}\n Body: {}'.format(request.method, request.url, request.json))
-            result = fn(*args, **kwargs)
-            return result
+            return fn(*args, **kwargs)
         except Exception as ex:
             app.logger.debug("Exception {0}".format(ex))
             raise ex
+
     return decorated
