c586336dc38738b8818ad47b4d7042b38a3c419d
Oleksii Vovkoboi <ovovkoboi@griddynamics.com>
2020-05-11 12:45:41 +0200

    - add access tokens audit;

--- api/login_api.py
+++ api/login_api.py
@@ -3,7 +3,7 @@ import datetime
 from flask import Blueprint, make_response
 
 from decorators import *
-from models import Center
+from models import *
 
 login_api = Blueprint('login_api', __name__)
 
@@ -13,8 +13,11 @@ login_api = Blueprint('login_api', __name__)
 @log_request
 def get_token():
     request_data = request.get_json()
-    user = Center.query.filter_by(login=request_data['login']).filter_by(password=request_data['password']).first()
-    if user is not None:
+    center = Center.query.filter_by(login=request_data['login']).filter_by(password=request_data['password']).first()
+    if center is not None:
+        access_log_record = AccessLog(center_id=center.id, time_stamp=datetime.datetime.utcnow())
+        db.session.add(access_log_record)
+        db.session.commit()
         expiration_date = datetime.datetime.utcnow() + datetime.timedelta(minutes=app.config['SESSION_DURATION_MIN'])
         return jwt.encode({'exp': expiration_date}, app.config['SECRET_KEY'])
     else:
